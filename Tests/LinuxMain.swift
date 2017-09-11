98296f8cea75bb91fcbd3b6bfa63f68de6ac2679
Youming Lin <youminglin@outlook.com>
2017-09-11 14:50:12 -0500

    Fix warning and error on Linux

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -19,20 +19,37 @@ import XCTest
 @testable import HTMLEntitiesTests
 
 // Implementation taken from http://stackoverflow.com/a/24029847
-extension MutableCollection where Indices.Iterator.Element == Index {
-    mutating func shuffle() {
-        let c = count
-        guard c > 1 else { return }
+#if swift(>=3.2)
+    extension MutableCollection {
+        mutating func shuffle() {
+            let c = count
+            guard c > 1 else { return }
 
-        srand(UInt32(time(nil)))
-        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-            let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
-            guard d != 0 else { continue }
-            let i = index(firstUnshuffled, offsetBy: d)
-            swap(&self[firstUnshuffled], &self[i])
+            srand(UInt32(time(nil)))
+            for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+                let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
+                guard d != 0 else { continue }
+                let i = index(firstUnshuffled, offsetBy: d)
+                swapAt(firstUnshuffled, i)
+            }
         }
     }
-}
+#else
+    extension MutableCollection where Indices.Iterator.Element == Index {
+        mutating func shuffle() {
+            let c = count
+            guard c > 1 else { return }
+
+            srand(UInt32(time(nil)))
+            for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+                let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
+                guard d != 0 else { continue }
+                let i = index(firstUnshuffled, offsetBy: d)
+                swap(&self[firstUnshuffled], &self[i])
+            }
+        }
+    }
+#endif
 
 extension Sequence {
     func shuffled() -> [Iterator.Element] {
@@ -43,5 +60,5 @@ extension Sequence {
 }
 
 XCTMain([
-     testCase(HTMLEntitiesTests.allTests.shuffled()),
-].shuffled())
+    testCase(HTMLEntitiesTests.allTests.shuffled()),
+    ].shuffled())
