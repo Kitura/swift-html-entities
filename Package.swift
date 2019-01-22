a188de29934975bc42fd2a605f17e593f0a04f88
David Jones <djones6@uk.ibm.com>
2019-01-22 14:53:44 +0000

    Update to 4.2.1, support Swift 5, update Travis

--- Package.swift
+++ Package.swift
@@ -1,3 +1,6 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 /**
  * Copyright IBM Corporation 2016, 2017
  *
@@ -17,5 +20,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "HTMLEntities"
+    name: "HTMLEntities",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "HTMLEntities",
+            targets: ["HTMLEntities"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "HTMLEntities"
+        ),
+        .testTarget(
+            name: "HTMLEntitiesTests",
+            dependencies: ["HTMLEntities"]
+        )
+    ]
 )
